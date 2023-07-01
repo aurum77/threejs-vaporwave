8852f1a633433e19a8368641f572e867e1bb5cc0
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-07-01 22:41:08 +0300

    add the sun

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import * as THREE from "three";
 import grid from "./objects/grid";
 import camera from "./objects/camera";
 import scene from "./objects/scene";
+import sun from "./objects/sun";
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 scene.add(grid);
+scene.add(sun);
 
 function render() {
   renderer.render(scene, camera);
---
+++ objects/sun.js
@@ -0,0 +1,10 @@
+import * as THREE from "three";
+
+const geometry = new THREE.SphereGeometry( 15, 64, 64 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffd34e } );
+const sun = new THREE.Mesh( geometry, material );
+
+sun.position.x = 10
+sun.position.y = 30
+
+export default sun;

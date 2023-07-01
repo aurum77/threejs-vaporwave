1c6d26bc091b64017bed855a5473cb92788f842f
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-07-01 23:38:53 +0300

    remove slope because of bug

--- objects/grid.js
+++ objects/grid.js
@@ -9,10 +9,8 @@ const grid = new THREE.GridHelper(size, divs, color, color);
 
 grid.name = "vaporwave_grid";
 
-grid.rotation.z = -0.035;
-
 setInterval(() => {
-  grid.position.x -= 0.02;
+  grid.position.x -= 0.025;
 }, 1);
 
 export default grid;

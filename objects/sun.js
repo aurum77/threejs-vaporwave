7ee7f4ef3d682fedc5b7e05c39976ee04ce736b5
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-07-02 00:00:42 +0300

    sun color gradient

--- objects/sun.js
+++ objects/sun.js
@@ -1,10 +1,41 @@
 import * as THREE from "three";
 
-const geometry = new THREE.SphereGeometry(15, 64, 64);
-const material = new THREE.MeshBasicMaterial({ color: 0xffd34e });
+const geometry = new THREE.SphereGeometry(12, 128, 128);
+
+const material = new THREE.ShaderMaterial({
+  uniforms: {
+    color1: {
+      value: new THREE.Color(0xf4634a),
+    },
+    color2: {
+      value: new THREE.Color(0xffd34e),
+    },
+  },
+  vertexShader: `
+    varying vec2 vUv;
+
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+    }
+  `,
+  fragmentShader: `
+    uniform vec3 color1;
+    uniform vec3 color2;
+  
+    varying vec2 vUv;
+    
+    void main() {
+      
+      gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
+    }
+  `,
+  wireframe: false,
+});
+
 const sun = new THREE.Mesh(geometry, material);
 
-sun.position.x = 10;
+sun.position.x = -5;
 sun.position.y = 30;
 
 export default sun;

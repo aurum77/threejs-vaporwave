0ddbc5c9ceea76b10f0b84e60cc6dfb84213c352
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-07-01 22:39:30 +0300

    make grid color lighter

--- objects/grid.js
+++ objects/grid.js
@@ -3,7 +3,7 @@ import * as THREE from "three";
 const size = 90000;
 const divs = 20000;
 
-const color = new THREE.Color(0xfc3cfe);
+const color = new THREE.Color(0xfd3dff);
 
 const grid = new THREE.GridHelper(size, divs, color, color);
 

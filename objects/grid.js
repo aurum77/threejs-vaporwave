f0cfa964ec6a3be17e2aae721e9429c4bdfa43e1
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-07-01 22:40:52 +0300

    formatting

--- objects/grid.js
+++ objects/grid.js
@@ -7,12 +7,12 @@ const color = new THREE.Color(0xfd3dff);
 
 const grid = new THREE.GridHelper(size, divs, color, color);
 
-grid.name = "vaporwave_grid"
+grid.name = "vaporwave_grid";
 
-grid.rotation.z = -.01;
+grid.rotation.z = -0.01;
 
 setInterval(() => {
-  grid.position.x -= .02;
+  grid.position.x -= 0.02;
 }, 1);
 
 export default grid;
--- objects/scene.js
+++ objects/scene.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
-const scene = new THREE.Scene();
 
-const color = new THREE.Color(0x120435)
+const scene = new THREE.Scene();
+const color = new THREE.Color(0x220435);
 
 scene.background = color;
 

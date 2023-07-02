29ca12b102d524956c201bcdf18682a3704df583
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-07-02 12:29:12 +0300

    increase bloom amount

--- effects/bloomPass.js
+++ effects/bloomPass.js
@@ -4,7 +4,7 @@ import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js"
 // resolution, strength, radius, threshold
 const bloomPass = new UnrealBloomPass(
   new THREE.Vector2(window.innerWidth, window.innerHeight),
-  0.5,
+  0.7,
   0,
   0.11
 );
